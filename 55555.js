f5777dcd531f5e8354380480c455f1b61312bd7e
Ahmad Soliman <ahmadadel92@gmail.com>
2015-12-28 02:40:44 +0200

    init

---
+++ 55555.js
@@ -0,0 +1,8 @@
+["GAMfdounpHg", "LmoGdAB-n-U", "NXUcA-T5u8Y", "lDeeMGUgxTE", "ZaNBhAIGGOc", "5CjA8P_X0bs"].map(function(id){
+  d=document;
+  v=d.createElement('iframe');
+  v.width="340";
+  v.height="240";
+  v.src="https://www.youtube.com/embed/"+id+"?autoplay=1&vq=tiny";
+  d.body.insertBefore(v, d.body.firstChild);
+});
